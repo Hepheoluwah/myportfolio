c8e7620c1f101b9f5937f63165b02ad54741a2e8
Hepheoluwah <mtoyin27@gmail.com>
2025-01-09 11:02:21 +0100

    Completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
 
       <TransitionEffect />
       <main className="flex items-center text-dark w-full min-h-screen dark:text-light">
-        <Layout classsName="pt-0 md:pt-16 sm:pt-8 ml-[10rem]">
+        <Layout classsName="pt-0 md:pt-16 sm:pt-8">
           <div className="flex items-center justify-between w-full lg:flex-col">
             <div className="w1/2 md:w-full">
               <Image
