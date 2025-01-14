fb8146a91a8345ad0b2409731abc65142ea7ee91
Hepheoluwah <mtoyin27@gmail.com>
2025-01-15 00:26:24 +0100

    Completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,7 +51,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/IFEOLUWA  ADEBOLA  ADENIRAN.pdf"
+                  href="/IFEOLUWA ADENIRAN CV.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg 
                 font-semibold hover:bg-light hover:text-dark border border-solid border-transparent 
